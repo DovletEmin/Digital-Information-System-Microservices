c21914e28b2f6c239ecadaa6b5258d724b817ff3
Dowlet Eminow <dovlet.eminov26.02@gmail.com>
2026-02-05 13:40:16 +0500

    chore(systemd): update auth unit and deps

--- systemd/install-dependencies.sh
+++ systemd/install-dependencies.sh
@@ -57,6 +57,11 @@ fi
 
 echo ""
 echo "=== Установка PostgreSQL 15 ==="
+# Добавление официального репозитория PostgreSQL
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt update
+
 apt install -y postgresql-15 postgresql-contrib-15
 
 # Запуск PostgreSQL
